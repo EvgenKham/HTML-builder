6f3035dbe3268f33e0e9d1556538198abafb6b26
EvgenKh <hamitcevich@gmail.com>
2025-01-19 18:07:54 +0300

    feat: implement 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { copyFile, readdir } = require('fs');
+const { mkdir, rm } = require('fs').promises;
+
+const oldDirPath = path.resolve('04-copy-directory', 'files');
+const newDirPath = path.resolve('04-copy-directory', 'files-copy');
+
+filesCopy(oldDirPath, newDirPath);
+
+async function filesCopy(oldDirPath, newDirPath) {
+  await rm(newDirPath, { recursive: true });
+  await mkdir(newDirPath, { recursive: true });
+
+  readdir(oldDirPath, (err, files) => {
+    if (err) {
+      console.log(err);
+    }
+    files.forEach((file) => {
+      const oldFilePath = path.resolve(oldDirPath, file);
+      const newFilePath = path.resolve(newDirPath, file);
+
+      copyFile(oldFilePath, newFilePath, (err) => {
+        if (err) {
+          return console.error(`Error copying file: ${err}`);
+        }
+      });
+    });
+  });
+}
